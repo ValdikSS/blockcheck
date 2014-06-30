d5dc2f2357d9504ff755a143afba103d8b3644a6
ValdikSS <iam@valdikss.org.ru>
2014-06-30 18:25:07 +0400

    setup.py for cx_freeze

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from cx_Freeze import setup, Executable
+
+executables = [
+    Executable('blockcheck.py')
+]
+
+setup(name='blockcheck',
+      version='0.1',
+      description='BlockCheck',
+      executables=executables,
+      options = {'build_exe': {'init_script':'Console'}},
+      )
